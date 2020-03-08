680657395784889f9e71a5eba7fdf5b48f5db11f
장연호 <siabard@mac-siabard.local>
2020-03-08 14:23:30 +0900

    opengl 설정

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use std::ffi::{CStr, CString};
+mod render_gl;
+pub use render_gl::*;
+
+fn main() {
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(3, 3);
+
+    let window = video_subsystem
+        .window("Game", 900, 700)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
+
+    let gl_context = window.gl_create_context().unwrap();
+    let gl =
+        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    let mut game_running = true;
+
+    unsafe {
+        gl::Viewport(0, 0, 900, 700);
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
+
+    let vert_shader =
+        render_gl::Shader::from_vert_source(&CString::new(include_str!("triangle.vert")).unwrap())
+            .unwrap();
+    let frag_shader =
+        render_gl::Shader::from_frag_source(&CString::new(include_str!("triangle.frag")).unwrap())
+            .unwrap();
+
+    let shader_program = render_gl::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
+
+    shader_program.set_used();
+
+    while game_running == true {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => {
+                    game_running = false;
+                }
+                _ => {}
+            }
+        }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
+    }
+}
